8236c6853529f3ffe78407358c4eb569c9d9042e
ValentinHRoldan <valentinroldan223@gmail.com>
2025-04-05 15:07:15 -0300

    ta

--- apps/anuncio/api.py
+++ apps/anuncio/api.py
@@ -49,4 +49,22 @@ class AnuncioListaAPIView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-                  
+
+class AnuncioDetalleAPIView(APIView):
+    def get(self, request, pk, format=None):
+        anuncio = get_object_or_404(Anuncio, pk=pk)
+        serializer = AnuncioSerializer(anuncio)
+        return Response(serializer.data)
+    
+    def patch(self, request, pk, format=None):
+        anuncio = get_object_or_404(Anuncio, pk=pk)
+        serializer = AnuncioSerializer(anuncio, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        anuncio = get_object_or_404(Anuncio, pk=pk)
+        anuncio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- apps/anuncio/urls.py
+++ apps/anuncio/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .api import CategoriaListaAPIView, CategoriaDetalleAPIView, AnuncioListaAPIView
+from .api import CategoriaListaAPIView, CategoriaDetalleAPIView, AnuncioListaAPIView, AnuncioDetalleAPIView
 
 app_name = 'anuncio'
 urlpatterns = [
     path('api/categoria/', CategoriaListaAPIView.as_view()),
     path('api/categoria/<pk>/', CategoriaDetalleAPIView.as_view()),
-    path('api/anuncios/', AnuncioListaAPIView.as_view()),
+    path('api/anuncio/', AnuncioListaAPIView.as_view()),
+    path('api/anuncio/<pk>/', AnuncioDetalleAPIView.as_view()),
 ]
