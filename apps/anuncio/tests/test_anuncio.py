8cb37c47da026a20cb359b6e12c6875bc8b7fc93
Acosta-Marcos <acostamarlih9@gmail.com>
2025-06-11 20:45:16 -0300

    Update test_anuncio.py

--- apps/anuncio/tests/test_anuncio.py
+++ apps/anuncio/tests/test_anuncio.py
@@ -3,9 +3,11 @@ import pytest
 from django.urls import reverse
 from django.utils import timezone
 from apps.usuario.tests.conftest import create_user, create_superuser, test_password, grupo_usuarios_registrados
-from apps.anuncio.models import Anuncio, Categoria
+from apps.anuncio.models import Anuncio, Categoria, OfertaAnuncio
 from rest_framework import status
-
+from .fixtures_user import get_authenticated_client, get_user_generico, api_client, api_client
+from .fixtures_categoria import get_categoria
+from .fixtures_anuncio import get_anuncio, get_user_generico_anuncio
 
 # @pytest.mark.django_db
 # def test_creacion_anuncio(api_client, create_superuser, test_password):
@@ -167,3 +169,98 @@ def test_oferta_fallida_por_ser_el_creador(api_client, create_user, crear_anunci
     # 4.2. Creación fallida de una Oferta de Anuncio por contener datos inválidos
     # 4.3. Creación fallida de una Oferta de Anuncio porque el usuario que intenta ofertar es el creador del mismo ✅
     # anuncio.
+
+
+def test_foo():
+    assert True
+
+
+def test_lista():
+    assert list(reversed([1, 2, 3])) == [3, 2, 1]
+
+@pytest.mark.django_db
+def test_api_creacion_anuncio(get_authenticated_client): #1.1
+    client = get_authenticated_client
+
+    data = {
+        "nombre": "Equipo deportivo",
+    }
+
+    response = client.post(f'/api/categoria/', data=data)
+
+    data = {
+        "titulo": "par de mancuernas",
+        "descripcion": "de 5kg c/u",
+        "precio_inicial": "30000",
+        "fecha_inicio": "2025-06-15",
+        "fecha_fin": "2025-06-30",
+        "categorias_ids": [
+            1
+        ]
+    }
+    print(client._credentials)
+    response = client.post(f'/api/anuncio/', data=data)
+    assert response.status_code == 201
+    assert Anuncio.objects.filter(titulo='par de mancuernas').count() == 1
+
+@pytest.mark.django_db
+def test_api_mod_anuncio(get_authenticated_client,get_categoria): #2.1
+
+    client = get_authenticated_client
+
+    data = {
+        "titulo": "par de mancuernas",
+        "descripcion": "de 5kg c/u",
+        "precio_inicial": "30000",
+        "fecha_inicio": "2025-06-15",
+        "fecha_fin": "2025-06-30",
+        "categorias_ids": [
+            1
+        ]
+    }
+
+    response = client.post(f'/api/anuncio/', data=data)
+
+    data = {
+        "descripcion": "de 5.5kg c/u",
+        "precio_inicial": "32000",
+    }
+    print(client._credentials)
+    response = client.patch(f'/api/anuncio/1/', data=data)
+    assert response.status_code == 200
+    assert Anuncio.objects.filter(titulo='par de mancuernas').count() == 1
+
+@pytest.mark.django_db
+def test_api_lista_anuncios(get_authenticated_client, get_anuncio): #3.1
+    cliente = get_authenticated_client
+
+    anuncio = get_anuncio
+
+    response = cliente.get(f'/api/anuncio/')
+    assert response.status_code == 200
+
+    data = response.data
+    assert data[0]['titulo'] == anuncio.titulo
+
+@pytest.mark.django_db
+def test_api_creacion_oferta_anuncio(get_authenticated_client, get_categoria, get_anuncio): #4.1
+    client = get_authenticated_client
+
+    data = {
+        "precio_oferta": "150001"
+    }
+
+    response = client.post(f'/api/anuncio/1/ofertar/', data=data)
+    assert response.status_code == 201
+    assert OfertaAnuncio.objects.filter(precio_oferta='150001',anuncio=get_anuncio).count() == 1
+
+
+@pytest.mark.django_db
+def test_api_fallo_creacion_oferta_anuncio_datos_invalidos(get_authenticated_client, get_categoria, get_anuncio): #4.2
+    client = get_authenticated_client
+
+    data = {
+        "precio_oferta": "149999.99"
+    }
+
+    response = client.post(f'/api/anuncio/1/ofertar/', data=data)
